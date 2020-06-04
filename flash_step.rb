e4237e0566122bc9612623329707b309d4ad84a9
Sunny0022 <Sunny0022@users.noreply.github.com>
2020-06-04 15:35:39 +0530

    Create flash_step.rb

---
+++ flash_step.rb
@@ -0,0 +1,20 @@
+require_relative '../pages/flash_page.rb'
+
+Given(/^We are on FlashSale signup form$/) do
+  @create = Createuser.new
+  @create.load
+end
+
+When(/^I fill the form$/) do
+  @create.populate
+  (
+  Name :Test
+  Email :'test_me@domain.com'
+  Password :'Test123,./'
+  Confirm_password :'Test123,./'
+  )
+end
+
+Then(/^I display result$/) do
+  @create.send
+end
