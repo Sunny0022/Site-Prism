26dfa8af6ed392d259bce2b688f151a7b76e3772
Sunny0022 <Sunny0022@users.noreply.github.com>
2020-06-04 15:39:11 +0530

    Create flash_page.rb

---
+++ flash_page.rb
@@ -0,0 +1,22 @@
+require 'site_prism'
+require 'rspec'
+class FlashPage < SitePrism::Page
+  set_url 'http://shubham.domain4now.com/users/new'
+
+  element :Name, '[id="user_name"]'
+  element :Email, '[id="user_email"]'
+  element :Password, '[id="user_password"]'
+  element :Confirm_password, '[id="user_password_confirmation"]'
+  element :button, 'button[type="submit"]'
+
+  def populate(**attrs)
+    Name.set attrs[:Name]
+    Email.set attrs[:Email]
+    Password.set attrs[:Password]
+    Confirm_password.set attrs[:Confirm_password]
+  end
+
+  def send
+    button.click
+  end
+end
